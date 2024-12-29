f8ef6738093986f62c035c75d902f10fce745911
Philipp Sommerhalter <philipp.sommerhalter@payaut.com>
2024-12-29 22:19:09 +0100

    Improvements to release

--- README.md
+++ README.md
@@ -1,44 +1,16 @@
 # calabash-sat
 Calabash SAT is a tool to analyze green patterns within a collection of projects. Using semgrep as a rule matching engine, it allows users to provide a set of rules, and grade multiple projects against each other.
 
-## Download and Install
+## Installation
 
-1. **Go to the Releases Page**:
-   - Navigate to the [Releases page](https://github.com/philippsommer27/calabash-sat/releases).
+0. **Install semgrep cli**:
+    - On Mac this is most easily done throw homebrew: `brew install semgrep`
+    - For others platforms, see [semgrep README](https://github.com/semgrep/semgrep?tab=readme-ov-file#option-2-getting-started-from-the-cli)
 
-2. **Download the Appropriate Binary**:
-   - Download the binary for your operating system:
-     - `calabash-sat_<version>_windows_amd64.zip` for Windows
-     - `calabash-sat_<version>_linux_amd64.tar.gz` for Linux
-     - `calabash-sat_<version>_darwin_amd64.tar.gz` for macOS
+1. [**Install Go**](https://go.dev/doc/install)
 
-3. **Extract the Binary**:
-   - For Windows:
-     - Right-click the downloaded `.zip` file and select "Extract All".
-   - For Linux/macOS:
-     - Use the terminal to extract the `.tar.gz` file:
-       ```bash
-       tar -xzf calabash-sat_<version>_linux_amd64.tar.gz
-       ```
-
-4. **Add the Binary to PATH** (Optional):
-   - **Windows**:
-     - Copy the extracted `calabash-sat.exe` to a directory of your choice (e.g., `C:\Program Files\CalabashSAT`).
-     - Add this directory to your PATH:
-       - Open the Start Menu, search for "Environment Variables", and select "Edit the system environment variables".
-       - In the System Properties window, click on the "Environment Variables" button.
-       - In the Environment Variables window, find the "Path" variable in the "System variables" section and select it.
-       - Click "Edit", then "New", and add the path to the directory where you copied the binary (e.g., `C:\Program Files\CalabashSAT`).
-       - Click "OK" on all windows to apply the changes.
-   - **Linux/macOS**:
-     - Move the binary to `/usr/local/bin`:
-       ```bash
-       sudo mv calabash-sat /usr/local/bin/
-       ```
-     - Ensure the binary is executable:
-       ```bash
-       sudo chmod +x /usr/local/bin/calabash-sat
-       ```
+2. **Install Calabash SAT**
+    - `go install github.com/philippsommer27/calabash-sat@latest`
 
 5. **Run the CLI Application**:
    - Open a terminal.
@@ -50,7 +22,7 @@ Calabash SAT is a tool to analyze green patterns within a collection of projects
 Using this tool involves two phases, corresponding to the two commands available.
 
 ## Analyze rules within a set of projects.
-You must use the tool to determine the prevelance of projects in your project dataset. To do this use the following command: `evalrule <path to rule directory> <path to projects directory> <path output directory>`
+You must use the tool to determine the prevelance of projects in your project dataset. To do this use the following command: `evalrule <path to rule directory> <path to projects directory> <path output directory> <language>`
 
 You may additionally use the `-P` tag to print semgreps output or `-M` to enable multithreading.
 
@@ -61,4 +33,5 @@ The second function allows you to calculate an overall grade for each project ba
 
 First place all your result files from the first command for each pattern into one folder. Then create a json file that maps the pattern to a severity weight between 1 and 3.
 
-Finally run the command `evalprojs <path to directory containing findings>`
+Finally run the command `evalprojs <path to severity ratings> <path to directory containing findings>`
+For an example of what the severity ratings should look like, see [here](https://github.com/philippsommer27/calabash-catalog/blob/main/catalog/static-analysis/Overall%20Results/testInfo.json)
--- internal/utils.go
+++ internal/utils.go
@@ -15,66 +15,66 @@ import (
 )
 
 func skipProjects(projects []fs.DirEntry, out string) []fs.DirEntry {
-    existingResults := make(map[string]struct{})
+	existingResults := make(map[string]struct{})
 
-    addToMap := func(dir string) {
-        files, err := os.ReadDir(dir)
-        if err != nil {
-            log.Fatalf("Error reading directory %s: %v", dir, err)
-        }
-        for _, file := range files {
-            if !file.IsDir() {
-                existingResults[strings.TrimSuffix(file.Name(), ".json")] = struct{}{}
-            }
-        }
-    }
+	addToMap := func(dir string) {
+		files, err := os.ReadDir(dir)
+		if err != nil {
+			log.Fatalf("Error reading directory %s: %v", dir, err)
+		}
+		for _, file := range files {
+			if !file.IsDir() {
+				existingResults[strings.TrimSuffix(file.Name(), ".json")] = struct{}{}
+			}
+		}
+	}
 
 	addToMap(out)
-    addToMap(filepath.Join(out, "no_findings"))
+	addToMap(filepath.Join(out, "no_findings"))
 
-    var filteredProjects []fs.DirEntry
-    for _, project := range projects {
-        if project.IsDir() {
-            if _, exists := existingResults[project.Name()]; !exists {
-                filteredProjects = append(filteredProjects, project)
-            }
-        }
-    }
-	fmt.Printf("Skipping %d projects with existing results\n", len(projects) - len(filteredProjects))
-    return filteredProjects
+	var filteredProjects []fs.DirEntry
+	for _, project := range projects {
+		if project.IsDir() {
+			if _, exists := existingResults[project.Name()]; !exists {
+				filteredProjects = append(filteredProjects, project)
+			}
+		}
+	}
+	fmt.Printf("Skipping %d projects with existing results\n", len(projects)-len(filteredProjects))
+	return filteredProjects
 }
 
 func reportProgress(completed *int64, total int64) {
-    for {
-        completedItems := atomic.LoadInt64(completed)
-        percentComplete := float64(completedItems) / float64(total) * 100
-        fmt.Printf("\rProgress: %.2f%% (%d/%d)", percentComplete, completedItems, total)
-        
-        if completedItems == total {
-            fmt.Println()
-            return
-        }
-        
-        time.Sleep(10 * time.Millisecond)
-    }
+	for {
+		completedItems := atomic.LoadInt64(completed)
+		percentComplete := float64(completedItems) / float64(total) * 100
+		fmt.Printf("\rProgress: %.2f%% (%d/%d)", percentComplete, completedItems, total)
+
+		if completedItems == total {
+			fmt.Println()
+			return
+		}
+
+		time.Sleep(10 * time.Millisecond)
+	}
 }
 
 func getJSONFiles(dir string) []os.DirEntry {
-    files, err := os.ReadDir(dir)
-    if err != nil {
-        log.Fatalf("Error reading directory %s: %v", dir, err)
-    }
-    return files
+	files, err := os.ReadDir(dir)
+	if err != nil {
+		log.Fatalf("Error reading directory %s: %v", dir, err)
+	}
+	return files
 }
 
 func countJSONFiles(files []os.DirEntry) int {
-    count := 0
-    for _, file := range files {
-        if !file.IsDir() && strings.HasSuffix(file.Name(), ".json") {
-            count++
-        }
-    }
-    return count
+	count := 0
+	for _, file := range files {
+		if !file.IsDir() && strings.HasSuffix(file.Name(), ".json") {
+			count++
+		}
+	}
+	return count
 }
 
 func getLinesOfCode(dir, language string) int {
@@ -87,29 +87,31 @@ func getLinesOfCode(dir, language string) int {
 	processor := gocloc.NewProcessor(languages, options)
 	result, err := processor.Analyze(paths)
 	if err != nil {
-		fmt.Printf("gocloc fail. error: %v\n", err)
+		log.Fatalf("gocloc fail. error: %v", err)
 		return -1
 	}
 
 	for _, lang := range result.Languages {
-		if lang.Name == language {
+		if strings.EqualFold(lang.Name, language) {
 			return int(lang.Code)
 		}
 	}
+	log.Fatalf("Zero lines of code found in the selected language, exiting...")
+	os.Exit(1)
 	return 0
 }
 
 func ReadTestInfoFromFile(filePath string) ([]TestInfo, error) {
-    file, err := os.ReadFile(filePath)
-    if err != nil {
-        return nil, err
-    }
+	file, err := os.ReadFile(filePath)
+	if err != nil {
+		return nil, err
+	}
 
-    var testInfos []TestInfo
-    err = json.Unmarshal(file, &testInfos)
-    if err != nil {
-        return nil, err
-    }
+	var testInfos []TestInfo
+	err = json.Unmarshal(file, &testInfos)
+	if err != nil {
+		return nil, err
+	}
 
-    return testInfos, nil
+	return testInfos, nil
 }
